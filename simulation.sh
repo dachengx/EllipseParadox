48cb30b86d5c3254094b4a154dac2c8523f5bf33
xudacheng <xudachengthu@hotmail.com>
2020-01-05 14:27:49 +0000

    rm xa in EP2D2.wl

--- simulation.sh
+++ simulation.sh
@@ -19,12 +19,12 @@ wolframscript -file EP3D0.wl ${scale} ${scale} 1.1 1.2 1 0.05 0.05 "${path3D}/EP
 #wolframscript -file EP2D0.wl ${scale} ${scale} 1.1 1.2 1 0.05 0.05 "${path2D}/EP2D02.png"
 #wolframscript -file EP3D0.wl ${scale} ${scale} 1.1 1.2 1 0.05 0.05 "${path3D}/EP3D02.png"
 
-wolframscript -file EP2D1.wl ${scale} ${scale} 1.1 1 0.05 0.05 "${path2D}/EP2D10.png" >> log.txt
-wolframscript -file EP3D1.wl ${scale} ${scale} 1.1 1 0.05 0.05 "${path3D}/EP3D10.png" >> log.txt
+wolframscript -file EP2D1.wl ${scale} ${scale} 1.2 1 0.05 0.05 "${path2D}/EP2D10.png" >> log.txt
+wolframscript -file EP3D1.wl ${scale} ${scale} 1.2 1 0.05 0.05 "${path3D}/EP3D10.png" >> log.txt
 #wolframscript -file EP2D1.wl ${scale} ${scale} 1.1 1 0.05 0.05 "${path2D}/EP2D11.png"
 #wolframscript -file EP3D1.wl ${scale} ${scale} 1.1 1 0.05 0.05 "${path3D}/EP3D11.png"
 
-#wolframscript -file EP2D2.wl ${scale} ${scale} 1 0.05 0.05 "${path2D}/EP2D20.png"
+wolframscript -file EP2D2.wl ${scale} ${scale} 1 0.05 0.05 "${path2D}/EP2D20.png" >> log.txt
 #wolframscript -file EP3D2.wl ${scale} ${scale} 1 0.05 0.05 "${path3D}/EP3D20.png"
 
 #wolframscript -file EP2D3.wl ${scale} ${scale} 1.1 0.05 0.05 "${path2D}/EP2D30.png"
